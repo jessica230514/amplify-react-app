7bf0c3157f460ca2ffcd0f918e1c14933b92d324
jessica23 <jessik_jz@outlook.es>
2024-03-27 12:44:46 -0600

    modification

--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import { withAuthenticator, Button, Heading, View, Card } from "@aws-amplify/ui-react";
-import axios from 'axios';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App({ user, signOut }) {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    if (user) {
-      callLambdaAPI();
-    }
-  }, [user]);
-
-  function callLambdaAPI() {
-    const apiEndpoint = 'https://gmvnr8jlj3.execute-api.us-east-1.amazonaws.com/default/nsjac-HelloWorldFunction-wb23HT9T4pb0';
-    axios.get(apiEndpoint)
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log('Error calling from Lambda: ', error);
-      });
-  }
-
+function App({ signOut }) {
   return (
     <View className="App">
       <Card>
-        <Heading level={1}>Data from Lambda!</Heading>
-        {data && (
-          <div>
-          <div>
-            
-          </div>
-            <table style={{ borderCollapse: 'collapse', width: '100%' }}>
-              <thead>
-                <tr>
-                  <th style={{ border: '1px solid black', padding: '8px' }}>Track Name</th>
-                  <th style={{ border: '1px solid black', padding: '8px' }}>Artist Names</th>
-                  <th style={{ border: '1px solid black', padding: '8px' }}>Streams</th>
-                  <th style={{ border: '1px solid black', padding: '8px' }}>Duration (ms)</th>
-                  {/* Add more headers as needed */}
-                </tr>
-              </thead>
-              <tbody>
-                {data.map(item => (
-                  <tr key={item.id}>
-                    <td style={{ border: '1px solid black', padding: '8px' }}>{item.track_name}</td>
-                    <td style={{ border: '1px solid black', padding: '8px' }}>{item.artist_names}</td>
-                    <td style={{ border: '1px solid black', padding: '8px' }}>{item.streams}</td>
-                    <td style={{ border: '1px solid black', padding: '8px' }}>{item.duration_ms}</td>
-                    {/* Add more cells as needed */}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
